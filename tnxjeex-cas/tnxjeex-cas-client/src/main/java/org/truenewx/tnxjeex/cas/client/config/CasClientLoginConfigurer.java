b90ce540bede91838ed0c34e0ce6be2870b4ad65
projavaer <pro_javaer@163.com>
2020-05-14 11:50:40 +0800

    抽取CAS客户端通用的登录配置器

---
+++ tnxjeex-cas/tnxjeex-cas-client/src/main/java/org/truenewx/tnxjeex/cas/client/config/CasClientLoginConfigurer.java
@@ -0,0 +1,35 @@
+package org.truenewx.tnxjeex.cas.client.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.cas.authentication.CasAuthenticationProvider;
+import org.springframework.security.cas.web.CasAuthenticationFilter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.stereotype.Component;
+import org.truenewx.tnxjee.web.api.meta.model.ApiMetaProperties;
+import org.truenewx.tnxjee.web.security.config.SecurityLoginConfigurerSupport;
+import org.truenewx.tnxjeex.cas.client.filter.CasClientAuthenticationFilter;
+
+/**
+ * CAS客户端登录配置器
+ */
+@Component
+public class CasClientLoginConfigurer
+        extends SecurityLoginConfigurerSupport<CasAuthenticationProvider> {
+
+    @Autowired(required = false)
+    private ApiMetaProperties apiMetaProperties;
+    @Autowired
+    private RedirectStrategy redirectStrategy;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        CasClientAuthenticationFilter filter = new CasClientAuthenticationFilter();
+        filter.setRedirectStrategy(this.redirectStrategy);
+        filter.setSuccessTargetUrlParameter(this.apiMetaProperties.getLoginSuccessRedirectParameter());
+        filter.setAuthenticationManager(http.getSharedObject(AuthenticationManager.class)); // 固定必须
+        http.addFilterAt(filter, CasAuthenticationFilter.class);
+    }
+
+}
