537239d627df39f327e623f6cbb5fe4bbd1c747b
projavaer <pro_javaer@163.com>
2020-05-19 17:29:49 +0800

    Assertion反序列化恢复为通过简单实现类实现，以支持session的反序列化

--- tnxjeex-cas/tnxjeex-cas-client/src/main/java/org/truenewx/tnxjeex/cas/client/validation/CasJsonServiceTicketValidator.java
+++ tnxjeex-cas/tnxjeex-cas-client/src/main/java/org/truenewx/tnxjeex/cas/client/validation/CasJsonServiceTicketValidator.java
@@ -4,17 +4,12 @@ import org.apache.commons.lang3.StringUtils;
 import org.jasig.cas.client.validation.AbstractCasProtocolUrlBasedTicketValidator;
 import org.jasig.cas.client.validation.Assertion;
 import org.jasig.cas.client.validation.TicketValidationException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.truenewx.tnxjee.core.tools.ClassGenerator;
 import org.truenewx.tnxjee.core.util.JsonUtil;
 
 /**
  * 基于JSON数据格式的CAS服务票据校验器
  */
 public class CasJsonServiceTicketValidator extends AbstractCasProtocolUrlBasedTicketValidator {
-
-    @Autowired
-    private ClassGenerator classGenerator;
 
     public CasJsonServiceTicketValidator(String casServerUrlPrefix) {
         super(casServerUrlPrefix);
@@ -29,8 +24,7 @@ public class CasJsonServiceTicketValidator extends AbstractCasProtocolUrlBasedTi
     protected Assertion parseResponseFromServer(String response) throws TicketValidationException {
         Assertion assertion = null;
         if (StringUtils.isNotBlank(response)) {
-            Class<? extends Assertion> assertionClass = this.classGenerator.generateSimple(Assertion.class);
-            assertion = JsonUtil.json2Bean(response, assertionClass);
+            assertion = JsonUtil.json2Bean(response, SimpleAssertion.class);
         }
         if (assertion == null) {
             throw new TicketValidationException("The service ticket is invalid");
