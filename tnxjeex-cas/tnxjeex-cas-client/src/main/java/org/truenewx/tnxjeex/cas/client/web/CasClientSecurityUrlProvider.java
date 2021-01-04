2a3507dc254f7dd69b2316825ff9d8156bfd662c
jianglei <jianglei81@sina.cn>
2021-01-04 22:24:16 +0800

    改进安全地址提供者的自定义方式

--- tnxjeex-cas/tnxjeex-cas-client/src/main/java/org/truenewx/tnxjeex/cas/client/web/CasClientSecurityUrlProvider.java
+++ tnxjeex-cas/tnxjeex-cas-client/src/main/java/org/truenewx/tnxjeex/cas/client/web/CasClientSecurityUrlProvider.java
@@ -1,8 +1,12 @@
 package org.truenewx.tnxjeex.cas.client.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.truenewx.tnxjee.core.util.NetUtil;
 import org.truenewx.tnxjee.webmvc.security.web.SecurityUrlProvider;
 import org.truenewx.tnxjeex.cas.client.config.CasClientProperties;
@@ -10,14 +14,20 @@ import org.truenewx.tnxjeex.cas.client.config.CasClientProperties;
 /**
  * Cas客户端的安全地址提供者
  */
+@Component
 public class CasClientSecurityUrlProvider implements SecurityUrlProvider {
 
     @Autowired
     private CasClientProperties casClientProperties;
+    private Map<String, Object> params = new HashMap<>();
+
+    public void addParam(String name, Object value) {
+        this.params.put(name, value);
+    }
 
     @Override
     public String getDefaultLoginFormUrl() {
-        return this.casClientProperties.getLoginFormUrl();
+        return NetUtil.mergeParams(this.casClientProperties.getLoginFormUrl(), this.params, null);
     }
 
     @Override
@@ -29,7 +39,7 @@ public class CasClientSecurityUrlProvider implements SecurityUrlProvider {
 
     @Override
     public String getLogoutSuccessUrl() {
-        return this.casClientProperties.getLogoutSuccessUrl();
+        return NetUtil.mergeParams(this.casClientProperties.getLogoutSuccessUrl(), this.params, null);
     }
 
 }
