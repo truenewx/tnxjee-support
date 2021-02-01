403072a4ea8cd4906dde0cbc1e312e783baae027
jianglei <jianglei81@sina.cn>
2021-02-01 09:38:51 +0800

    配置器支持统一命名

--- tnxjeex-cas/tnxjeex-cas-server/src/main/java/org/truenewx/tnxjeex/cas/server/config/CasServerMvcConfigurerSupport.java
+++ tnxjeex-cas/tnxjeex-cas-server/src/main/java/org/truenewx/tnxjeex/cas/server/config/CasServerMvcConfigurerSupport.java
@@ -3,7 +3,10 @@ package org.truenewx.tnxjeex.cas.server.config;
 import org.sitemesh.builder.SiteMeshFilterBuilder;
 import org.truenewx.tnxjee.webmvc.view.config.WebViewMvcConfigurerSupport;
 
-public class CasServerMvcConfigSupport extends WebViewMvcConfigurerSupport {
+/**
+ * Cas服务端MVC配置器支持
+ */
+public class CasServerMvcConfigurerSupport extends WebViewMvcConfigurerSupport {
 
     @Override
     protected void buildSiteMeshFilter(SiteMeshFilterBuilder builder) {
