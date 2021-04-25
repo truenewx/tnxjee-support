809adfc3d87a1ef852a7f076f0782cee559451cd
jianglei <jianglei81@sina.cn>
2021-04-25 18:38:17 +0800

    根据本地存储路径是否配置的条件决定是否创建自有FSS访问器

--- tnxjeex-fss/tnxjeex-fss-service/src/main/java/org/truenewx/tnxjeex/fss/service/config/OwnFssServiceConfig.java
+++ tnxjeex-fss/tnxjeex-fss-service/src/main/java/org/truenewx/tnxjeex/fss/service/config/OwnFssServiceConfig.java
@@ -1,5 +1,6 @@
 package org.truenewx.tnxjeex.fss.service.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.truenewx.tnxjeex.fss.service.own.OwnFssAccessor;
@@ -11,6 +12,7 @@ import org.truenewx.tnxjeex.fss.service.own.OwnFssAccessor;
 public class OwnFssServiceConfig {
 
     @Bean
+    @ConditionalOnProperty("tnxjeex.fss.accessor.local.root")
     public OwnFssAccessor ownFssAccessor(FssLocalAccessorProperties properties) {
         return new OwnFssAccessor(properties.getRoot(), getOwnAccessorSalt());
     }
